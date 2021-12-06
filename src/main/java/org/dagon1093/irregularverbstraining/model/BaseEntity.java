fc49d4a31def38fc3d93c71dbf0d325d5603317b
dagon1093 <P0werofgood>
2021-12-06 08:10:10 +0300

    Создание базовой сущности. Закрыть #1

---
+++ src/main/java/org/dagon1093/irregularverbstraining/model/BaseEntity.java
@@ -0,0 +1,23 @@
+package org.dagon1093.irregularverbstraining.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Setter
+@Getter
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+}
