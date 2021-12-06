1a5864fa2fa7c34256daf732835b053761af13d5
dagon1093 <P0werofgood>
2021-12-06 07:48:35 +0300

    Первый коммит

---
+++ src/main/java/org/dagon1093/irregularverbstraining/IrregularVerbsTrainingApplication.java
@@ -0,0 +1,13 @@
+package org.dagon1093.irregularverbstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrregularVerbsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrregularVerbsTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dagon1093/irregularverbstraining/IrregularVerbsTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dagon1093.irregularverbstraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrregularVerbsTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
