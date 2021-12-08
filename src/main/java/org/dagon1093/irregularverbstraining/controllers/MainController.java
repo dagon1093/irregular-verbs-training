041bdba0ef48884bd2c4566ffa2294ca9f40ce9e
dagon1093 <P0werofgood>
2021-12-08 09:50:44 +0300

    Добавлен контроллер. Close #5

---
+++ src/main/java/org/dagon1093/irregularverbstraining/controllers/MainController.java
@@ -0,0 +1,13 @@
+package org.dagon1093.irregularverbstraining.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping({"","/"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
