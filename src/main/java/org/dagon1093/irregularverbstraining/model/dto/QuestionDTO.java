0fdceaa3019910d933690752fd70ffa955da687e
dagon1093 <P0werofgood>
2022-01-17 06:50:54 +0300

    Добавлена QuestionDTO. Close #19

---
+++ src/main/java/org/dagon1093/irregularverbstraining/model/dto/QuestionDTO.java
@@ -0,0 +1,17 @@
+package org.dagon1093.irregularverbstraining.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionDTO {
+
+    private List<String> words;
+}
