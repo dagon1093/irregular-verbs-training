829247f4025b5f99b3b320ae9fd65c135a4c2b1d
dagon1093 <P0werofgood>
2022-01-17 06:49:52 +0300

    Добавлена сущность Question. Close #17

---
+++ src/main/java/org/dagon1093/irregularverbstraining/model/Question.java
@@ -0,0 +1,19 @@
+package org.dagon1093.irregularverbstraining.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashMap;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    private HashMap<String, String> wordsMap;
+
+
+}
