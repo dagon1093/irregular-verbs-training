7972390a310b8cf0e3779f4ab85e0d4e7b5e7dce
dagon1093 <P0werofgood>
2022-01-20 02:08:03 +0300

    Добавлена сущность Answer. Close #21

---
+++ src/main/java/org/dagon1093/irregularverbstraining/model/Answer.java
@@ -0,0 +1,14 @@
+package org.dagon1093.irregularverbstraining.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+    private String answer;
+}
